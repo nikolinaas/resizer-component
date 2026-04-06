276cc6be763fe55ef0154887294b731ae88225dd
nikolinas <nikolina.s@aleapartners.io>
2026-04-06 15:18:11 +0200

    Updates

---
+++ precache-manifest.772c739a828012b2d2d70cc398f9c024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f393fc8e0b6860393c1313c460d506b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97169816817d29289aad",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "c12996e11eddf7330438",
+    "url": "./static/js/2.b3c23a87.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b3c23a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97169816817d29289aad",
+    "url": "./static/js/main.7f040129.chunk.js"
+  },
+  {
+    "revision": "f61dafe81a7ef20c88c4",
+    "url": "./static/js/runtime-main.52bee0a7.js"
+  }
+]);
