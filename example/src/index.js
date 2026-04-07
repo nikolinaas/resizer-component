865b3672a324c81415575e8b00c119817376e819
nikolinas <nikolina.s@aleapartners.io>
2026-04-07 08:42:22 +0200

    added index.d.ts

--- example/src/index.js
+++ example/src/index.js
@@ -3,5 +3,15 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+// import { ExampleComponent } from '.';
+
+declare module '@resizer-component/iframe-resizer' {
+
+    import * as React from 'react'
+
+    namespace ExampleComponent {
+
+    }
+
+function ExampleComponent(
+    // props: IframeResizer.IframeResizerProps,
+  ): React.ReactElement
+
+  export = ExampleComponent
+}
